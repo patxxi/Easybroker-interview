63d03506a423bcc805451a96d46d78f73bc84348
Francisco Ruiz <fruiz.rc@gmail.com>
2021-12-21 15:58:52 -0400

    add: test to get property detail

--- easybroker_api/tests/test_properties_api.py
+++ easybroker_api/tests/test_properties_api.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from easybroker_api.api import get_properties
+from easybroker_api.api import get_properties, get_property_detail
 
 
 class TestProperties(TestCase):
@@ -13,3 +13,10 @@ class TestProperties(TestCase):
 
         self.assertEqual(request.status_code, 200)
         self.assertEqual(len(data['content']), 15)
+
+    def test_get_property_detail(self):
+        request = get_property_detail('EB-C0156')
+        data = request.json()
+
+        self.assertEqual(request.status_code, 200)
+        self.assertEqual(data['public_id'], 'EB-C0156')
